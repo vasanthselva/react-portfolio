2dba22add78ae18b586067e2aa62ec07cc5fb177
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-04 22:39:48 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,11 +28,11 @@ export default function Header() {
             {toggleMenu && (
                 <nav className="block md:hidden">
                     <ul className="flex flex-col justify-content: center mobile-nav">
-                        <li><a href="/" onClick={handleLinkClick}>Home</a></li>
-                        <li><a href="#about" onClick={handleLinkClick}>About</a></li>
-                        <li><a href="#projects" onClick={handleLinkClick}>Projects</a></li>
-                        <li><a href="#experience" onClick={handleLinkClick}>Experience</a></li>
-                        <li><a href="#contact" onClick={handleLinkClick}>Contact</a></li>
+                        <li className="nav-item"><a href="/" onClick={handleLinkClick}>Home</a></li>
+                        <li className="nav-item"><a href="#about" onClick={handleLinkClick}>About</a></li>
+                        <li className="nav-item"><a href="#projects" onClick={handleLinkClick}>Projects</a></li>
+                        <li className="nav-item"><a href="#experience" onClick={handleLinkClick}>Experience</a></li>
+                        <li className="nav-item"><a href="#contact" onClick={handleLinkClick}>Contact</a></li>
                     </ul>
                 </nav>
             )}
