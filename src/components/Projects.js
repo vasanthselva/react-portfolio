a0147690613c4107f5c9d6131026298237b63be7
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-04 19:17:00 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,9 +2,32 @@ import Projects1Img from '../assets/chatbot.jpeg';
 import Projects2Img from '../assets/sign language.jpeg';
 import Projects3Img from '../assets/old portfolio.PNG';
 import Projects4Img from '../assets/new portfolio.PNG';
-import { FaGithub } from "react-icons/fa";
 export default function Projects() {
-    return(
+    const config = {
+        projects : [
+            {
+                image: Projects1Img,
+                discribtion:'The GYM Chatbot web appilication using NLP',
+                link:'https://github.com/vasanthselva/projects/tree/main/gym%20chatbot'
+            },
+            {
+                image: Projects2Img,
+                discribtion:'Sign language Recognition using python',
+                link:'https://github.com/vasanthselva/projects/tree/main/BATCH-5%20SIGN%20LANGUAGE%20DETECTION'
+            },
+            {
+                image: Projects3Img,
+                discribtion:'Portfolio website using basic html & css',
+                link:'https://vasanthselvaportfolio.netlify.app/'
+            },
+            {
+                image: Projects4Img,
+                discribtion:'Portfolio website using react library',
+                link:'https://github.com/vasanthselva/react-portfolio'
+            }
+        ]
+    }
+return(
         <section className='flex flex-col py-20 px-5 justify-center bg.primary text-[#5de79b]' id='projects'>
 <div className='w-1/2'>
 <div className='flex justify-center'>
@@ -13,38 +36,18 @@ export default function Projects() {
 </div>
 <div className='w-full'>
 <div className='flex flex-col md:flex-row px-10 gap-5 '>
-    <div className='relative'>
-    <a target="_blank" href="https://github.com/vasanthselva/projects/tree/main/gym%20chatbot">
-<img className='h-[200px] w-[500px]' src= {Projects1Img}/>
-<div className='absolute left-0 right-0 bottom-0 top-0 bg-primary opacity-0 duration=500 hover:opacity-100'>
-    <p className='text-center px-5 py-5'>The GYM Chatbot web appilication using NLP</p><p className='text-center px-5 py-5'>view</p>
+    {config.projects.map((proc) => (
+        <div className='relative'>
+    <img className='h-[200px] w-[500px]' src= {proc.image}/>
+    <div className='absolute left-0 right-0 bottom-0 top-0 bg-primary opacity-0 duration=500 hover:opacity-100'>
+        <p className='text-center px-5 py-5'>{proc.discribtion}</p>
+        <div className='flex justify-center'>
+    <a className='btn' target='blank' href={proc.link}>view project</a>
 </div>
-</a>
-</div>
-<div className='relative'>
-<a target="_blank" href="https://github.com/vasanthselva/projects/tree/main/BATCH-5%20SIGN%20LANGUAGE%20DETECTION">
-<img className='h-[200px] w-[500px]' src= {Projects2Img}/>
-<div className='absolute left-0 right-0 bottom-0 top-0 bg-primary opacity-0 duration=500 hover:opacity-100'>
-    <p className='text-center px-5 py-5'>Sign language Recognition using python</p><p className='text-center px-5 py-5'>view</p>
-</div>
-</a>
-</div>
-<div className='relative'>
-<a target="_blank" href="https://github.com/vasanthselva/portfolio">
-<img className='h-[200px] w-[500px]' src= {Projects3Img}/>
-<div className='absolute left-0 right-0 bottom-0 top-0 bg-primary opacity-0 duration=500 hover:opacity-100'>
-    <p className='text-center px-5 py-5'>Portfolio website using basic html & css</p><p className='text-center px-5 py-5'>view</p>
-</div>
-</a>
-</div>
-<div className='relative'>
-<a>
-<img className='h-[200px] w-[500px]' src= {Projects4Img}/>
-<div className='absolute left-0 right-0 bottom-0 top-0 bg-primary opacity-0 duration=500 hover:opacity-100'>
-    <p className='text-center px-5 py-5'>Portfolio website using react library</p><p className='text-center px-5 py-5'>view</p>
-</div>
-</a>
-</div>
+    </div>
+    </div>
+
+))}
 </div>
 </div>
         </section>
