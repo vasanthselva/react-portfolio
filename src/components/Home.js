eeced2daa6a2e6e63c97b2123565bd0115065fc2
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-04 19:28:08 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import vasanthImg from '../assets/vasanth.jpg';
-import vasanthPdf from '../assets/current cv.pdf';
+import ResumePdf from '../assets/current CV.pdf';
 import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 export default function Home() {
+    const config = {
+        subtitle:'Im Frontend and Backend Developer',
+        link:ResumePdf
+    }
+ 
     return(<section className='flex flex-col md:flex-row px-8 py-28 bg-secondary justify-center' id='home'>
         <div className='md:w-1/2 flex flex-col'> 
         <h1 className=' text-blue-950 text-4xl font-home-font justify-center'>Hi,<br/>Im <span className='text-white'>Vasanth S</span>
-        <p className='text-2xl justify-center'>Im Frontend and Backend Developer</p>
+        <p className='text-2xl justify-center'>{config.subtitle}</p>
         </h1>
         <div className='flex py-10 bg-icon'>
             <a href='https://github.com/vasanthselva'className='pr-5 '><FaGithub size={40} /></a>
@@ -14,7 +19,7 @@ export default function Home() {
         </div>
         <div className='md:w-1/2 flex flex-col'>
             <h1 className='text-4xl text-[#0b192e] font-home-font justify-center border-b-4 border-[#162942] mb-5 w-[125px] font-bold'>Resume</h1>
-                    <p className='pb-5 text-[#162942] font-home-font'>You can view my resume <a className='btn' href='#current cv.pdf' src={vasanthPdf}>Downlord</a></p>
+                    <p className='pb-5 text-[#162942] font-home-font'>You can view my resume <a className='btn' href={config.link}>Downlord</a></p>
             </div>
         </div>
         <img className='md:w-1/3' src={vasanthImg}/>
