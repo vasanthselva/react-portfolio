7ce60c7c4d886ec19837eacf67656bbe205b3370
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-05 12:41:45 +0530

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,9 +5,9 @@ export default function Experience() {
             <div className='md:w-full flex flex-col justify-center bg-text'>
             <h1 className='text-4xl text-[#0b192e] border-b-4 border-[#9cedc0] mb-5 w-[180px] font-bold '>Experience</h1>
             <h2 className='text-1xl text-[#0b192e]  font-bold '><FaIndustry/> Lucas TVS, Chennai</h2>
-          <p className='pb-5'>Feb 2024 - May 2024 Internship • (4 months)</p>
-          <p className='pb-5'>I done internship in “lucas tvs driven” for gain manufacturing company production knowledge.</p>
-          <p className='pb-5'>During my three-month internship at Lucas TVS, I gained comprehensive hands-on experience within a leading manufacturing company specializing in automotive components. My role involved:</p>
+          <p className='pb-5 font-home-font'>Feb 2024 - May 2024 Internship • (4 months)</p>
+          <p className='pb-5 font-home-font'>I done internship in “lucas tvs driven” for gain manufacturing company production knowledge.</p>
+          <p className='pb-5 font-home-font'>During my three-month internship at Lucas TVS, I gained comprehensive hands-on experience within a leading manufacturing company specializing in automotive components. My role involved:</p>
           <p className='pb-5'>Production Processes: Actively participated in daily production activities, including the assembly and testing of automotive parts. I observed and assisted in optimizing production workflows to enhance efficiency and reduce downtime.</p>
             
           <p className='pb-5'>Quality Control: Engaged in quality control procedures, learning to identify defects and ensure that products met stringent industry standards. I was involved in conducting inspections, performing tests, and documenting results to support continuous improvement initiatives.</p>
@@ -19,7 +19,7 @@ export default function Experience() {
           <p className='pb-5'>Technical Skills: Acquired practical knowledge in operating manufacturing machinery and utilizing quality control tools. Gained familiarity with industry software used for monitoring production metrics and managing quality data.</p>
             
           <h2 className='text-1xl text-[#0b192e]  font-bold pb-2'>DLK Career Development</h2>
-            <p>Industry visit Certificate for “React” domain from DLK career development participated and
+            <p className="font-home-font">Industry visit Certificate for “React” domain from DLK career development participated and
                 focused on React development, gaining practical insights into modern web development
                 practices and technologies acquired valuable knowledge.</p>
                 <p>During the visit, I engaged in a comprehensive program designed to enhance understanding and practical skills in modern web development. The visit provided valuable insights into:</p>
@@ -28,7 +28,7 @@ export default function Experience() {
                     <p className='pb-5'>Modern Web Development Practices: Learning about current industry practices, including best practices for building scalable and efficient web applications.</p>
                     <p className='pb-5'>Technological Trends: Understanding the latest trends and innovations in web development, and how they impact the use of React and related technologies. Practical Applications: Applying theoretical knowledge in real-world scenarios, and working on practical projects that demonstrate React’s capabilities in developing dynamic and interactive user interfaces.</p>
                     <h2 className='text-1xl text-[#0b192e]  font-bold pb-2'>Nimatooz Smile Mobility Private Limited</h2>
-              <p>Industrial visit: web Application development different modules of company projects and software services based works from other country’s. From Nimatooz smile mobility private limited. Founded with the vision of leveraging technology to enhance business efficiency, Nimatooz Smile Mobility has grown to become a significant contributor to the global IT landscape. Their projects span multiple sectors, demonstrating their versatility and adaptability in addressing diverse client needs.
+              <p className="font-home-font">Industrial visit: web Application development different modules of company projects and software services based works from other country’s. From Nimatooz smile mobility private limited. Founded with the vision of leveraging technology to enhance business efficiency, Nimatooz Smile Mobility has grown to become a significant contributor to the global IT landscape. Their projects span multiple sectors, demonstrating their versatility and adaptability in addressing diverse client needs.
                 Web Application Development</p>
             </div>
             
