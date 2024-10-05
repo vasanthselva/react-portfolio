a3d5ea604e18e9737452ad0a57ef6d827d1ceeb4
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-06 02:06:04 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,12 +41,12 @@ export default function Contact() {
     };
 
     return (
-        <section className='flex flex-col md:flex-row px-5 py-32 bg-secondary justify-center'id='contact'>
+        <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'id='contact'>
             <div className='md:w-1/2 flex justify-center'>
                 <img className='w-[250px] pb-5' src={ContactImg} alt="Contact" />
             </div>
             <div className='md:w-1/2 flex flex-col justify-center'>
-                <h1 className='text-4xl text-white border-b-4 border-[#5de79b] mb-5 w-[135px] font-bold'>Contact</h1>
+                <h1 className='text-4xl text-white border-b-4 border-[#8d274d] mb-5 w-[135px] font-bold'>Contact</h1>
                 <form onSubmit={sendGmail} className="flex flex-col space-y-4">
                     <input 
                         type="text" 
@@ -74,7 +74,7 @@ export default function Contact() {
                     />
                     <button 
                         type="submit" 
-                        className="p-3 con rounded-lg  transition duration-300">
+                        className="p-3 btn rounded-lg  transition duration-300">
                         Send Message
                      </button>
                 </form> 
