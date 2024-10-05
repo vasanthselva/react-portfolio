80580ac98d8f8d888972c49001efa1b5af471757
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-05 14:17:36 +0530

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -11,7 +11,7 @@ export default function Education() {
             </div>
             <div className='md:w-1/2 flex justify-center bg-text'>
             <div className='md:w-1/2 flex flex-col justify-center bg-'>
-            <h1 className='text-3xl text-[#0b192e] border-b-4 border-[#b32594cc] mb-5 w-[165px] font-bold'>Education</h1>
+            <h1 className='text-3xl text-[#0b192e] border-b-4 border-[#b32594cc] mb-5 w-[140px] font-bold'>Education</h1>
             <p className='pb-5 font-home-font'>{config.line1}<br/>
             {config.line2}</p>
             <h1 className='text-3x2 text-[#0b192e] mb-5 w-[75px] font-bold'>Batch</h1>
