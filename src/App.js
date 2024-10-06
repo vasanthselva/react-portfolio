041fe71bcaa0af7197fb500f663274e74ade3d73
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-06 12:15:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
+import Remote from './components/remote';
 import Footer from './components/Footer';
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Projects/>
       <Experience/>
       <Contact/>
+      <Remote/>
       <Footer/>
     </div>
   );
