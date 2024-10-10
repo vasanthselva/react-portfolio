03d28ed55bc36e931d0b4ba74fe9c323f923ff44
vasanth <123473435+vasanthselva@users.noreply.github.com>
2024-10-10 19:48:27 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <div className="py-4 bg-forth text-center text-white">&copy; Vasanth S {new Date().getFullYear()}</div>
+        <div className="py-4 bg-forth text-center text-black">&copy; Vasanth S {new Date().getFullYear()} All rights reserved.</div>
 
     );
 }
